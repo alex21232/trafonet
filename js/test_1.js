3447667c3cd735f46db93f639cbb63a6f46ecd21
Alejandro <milkozbigniew@gmail.com>
2023-12-06 10:43:29 -0400

    commit

---
+++ js/test_1.js
@@ -0,0 +1,26 @@
+// let num = 10
+// let num2 = 5
+
+// console.log(num + num2)
+// console.log(num * num2)
+// console.log(num - num2)
+// console.log(num / num2)
+
+// let boton = document.getElementById("btn-r")
+
+//boton.addEventListener("click", () => {
+//    window.alert('Hola, te robamos')
+//})
+
+// Igual igual ==
+// <= Menor o igual que
+// >= Mayor o igual que
+// > Mayor
+// < Menor
+// != Diferente que
+
+// 2==2&&3==4
+
+// 2==2||3==4
+
+//Algebra Booleana//
